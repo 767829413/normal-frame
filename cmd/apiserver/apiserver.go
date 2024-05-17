147730b99b2e4490423439ab6c557b712680d3ba
Fang Yuan <wojiushifangyuanlove@gmail.com>
2024-05-17 13:42:09 +0800

    update: some joker code
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- cmd/apiserver/apiserver.go
+++ cmd/apiserver/apiserver.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/767829413/normal-frame/internal/apiserver"
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
 	apiserver.NewApp("apiserver", "config").Run()
 }
